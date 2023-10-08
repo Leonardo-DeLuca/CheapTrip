258254a102f6efa9028d1c30a157317de1cec7f9
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-10-08 11:10:39 -0300

    Criada tela cadastro

--- app/src/main/java/com/example/cheaptrip/LoginActivity.java
+++ app/src/main/java/com/example/cheaptrip/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.cheaptrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView cadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cadastro = findViewById(R.id.mensagemCadastro);
+        cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(it);
+            }
+        });
     }
+
 }
