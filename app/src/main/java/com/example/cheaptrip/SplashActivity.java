bc1dc608eed26569b64ceeab739e960762f697b4
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-10-08 09:38:00 -0300

    Adicionado splash animation ao inicializar app

---
+++ app/src/main/java/com/example/cheaptrip/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.cheaptrip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView logo;
+    private Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo=findViewById(R.id.logo);
+        animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        logo.setAnimation(animation);
+        animation.start();
+    }
+}
