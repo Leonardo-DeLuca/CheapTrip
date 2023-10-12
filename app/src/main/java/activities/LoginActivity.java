cda96f0bdd2efc03710dd0cb6df6318403c992eb
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-10-12 13:41:42 -0300

    boadicionado funcionamento primario do tão manter conexão

--- app/src/main/java/activities/LoginActivity.java
+++ app/src/main/java/activities/LoginActivity.java
@@ -3,43 +3,84 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cheaptrip.R;
 
 import database.dao.UsuarioDAO;
 import database.model.UsuarioModel;
+import util.KeysUtil;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Definição de variáveis
     private TextView cadastro;
+    private CheckBox chkBoxManterConectado;
+    private Button loginBtn;
+    private SharedPreferences preferences;
+    private Boolean mantemConexao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        preferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+        SharedPreferences.Editor edit = preferences.edit();
 
-        cadastro = findViewById(R.id.mensagemCadastro);
-        cadastro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
-                startActivity(it);
-            }
-        });
+        mantemConexao = preferences.getBoolean(KeysUtil.MANTER_CONEXAO, false);
+        if(mantemConexao){
+            Intent it = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(it);
+        }
 
-        UsuarioModel u = new UsuarioModel();
-        u.setNomeCompleto("Vinicius");
-        u.setEmail("Vinicius@email.com");
-        u.setUsuario("vinicius");
-        u.setSenha("123");
+        else {
+            setContentView(R.layout.activity_login);
 
-        UsuarioDAO usuarioDAO = new UsuarioDAO(LoginActivity.this);
-        long insert = usuarioDAO.insert(u);
+            //Atribuições por id
+            chkBoxManterConectado = findViewById(R.id.chkBoxManterConectado);
+            cadastro = findViewById(R.id.mensagemCadastro);
+            loginBtn = findViewById(R.id.loginButton);
 
-        Log.d("Debuggandooo", String.valueOf(insert));
+            //Ação clicar novo cadasto
+            cadastro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Chama activity novo cadastro
+                    Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
+                    startActivity(it);
+                }
+            });
+
+            //Ação botão logar
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                //Chama função que realiza as validações de login.
+                public void onClick(View v) {
+                    acaoLogar(edit);
+                }
+            });
+
+        }
+
+    }
+
+    //Função que realiza as validações de login, verifica se irá manter-se conectado e entra no aplicativo.
+    private void acaoLogar (SharedPreferences.Editor edit){
+
+//        if(chkBoxManterConectado.isChecked()){
+//            edit.putBoolean(KeysUtil.MANTER_CONEXAO, true);
+//            edit.apply();
+//        }
+
+        Intent it = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(it);
+
     }
 
 }
---
+++ app/src/main/java/util/KeysUtil.java
@@ -0,0 +1,7 @@
+package util;
+
+public class KeysUtil {
+
+    public static final String MANTER_CONEXAO = "MANTER_CONEXAO";
+
+}
