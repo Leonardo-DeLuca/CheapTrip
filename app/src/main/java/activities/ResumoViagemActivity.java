bdb06a25f5726fe9e73e920d935414d4e09d6d17
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-10-26 11:19:05 -0300

    termidado mostrar informações individuais no resumo

--- app/src/main/java/activities/ResumoViagemActivity.java
+++ app/src/main/java/activities/ResumoViagemActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -17,6 +16,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
 import com.example.cheaptrip.R;
+
+import java.util.HashMap;
 
 import database.dao.EntretenimentoDAO;
 import database.dao.GasolinaDAO;
@@ -47,13 +48,20 @@ public class ResumoViagemActivity extends AppCompatActivity {
     private EntretenimentoModel entretenimentoModel;
 
     private ImageButton arrow1, arrow2, arrow3, arrow4, arrow5;
-    private LinearLayout hiddenView1, hiddenView2, hiddenView3, hiddenView4, hiddenView5;
+    private LinearLayout hiddenView1, hiddenView2, hiddenView3, hiddenView4, hiddenView5, resumoLinearEnt1, resumoLinearEnt2, resumoLinearEnt3;
     private CardView cardView1, cardView2, cardView3, cardView4, cardView5;
-    private TextView txtTotalKms, txtMediaKmsLitro, txtCustoMedioLitro, txtTotalVeiculos;
+    private TextView txtTotalKms, txtMediaKmsLitro, txtCustoMedioLitro, txtTotalVeiculos,
+            txtCustoPorPessoa, txtAluguelVeiculo,
+            txtCustoRefeicao, txtRefeicoesDia,
+            txtCustoMedioNoite, txtTotalNoites, txtTotalQuartos,
+            txtEntreterimento1, txtEntreterimento2, txtEntreterimento3, txtValorEntreterimento1, txtValorEntreterimento2, txtValorEntreterimento3,
+            txtTotalFinalGasolina, txtTotalFinalTarifasAereas, txtTotalFinalHospedagem, txtTotalFinalRefeicoes, txtTotalFinalEntreterimento;
 
     private LinearLayout[] hiddenViews;
     private CardView[] cardViews;
     private ImageButton[] imageButtons;
+
+    private HashMap<Integer, Object> map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,67 +84,120 @@ public class ResumoViagemActivity extends AppCompatActivity {
         imageButtons = new ImageButton[]{arrow1, arrow2, arrow3, arrow4, arrow5};
 
         for (int i = 0; i < hiddenViews.length; i++){
-            if(i == 0) {
-                if(gasolinaModel.getAdicionouViagem() != 0) {
-                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i]);
-                }
-                else{
-                    imageButtons[i].setAlpha(0.3F);
-                    imageButtons[i].setOnClickListener(view ->{
-                        Toast.makeText(ResumoViagemActivity.this, R.string.gasolinaAviso, Toast.LENGTH_SHORT).show();
-                    });
-                }
-            }
-            else if(i == 1) {
-                if(tarifaAereaModel.getAdicionouViagem() != 0) {
-                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i]);
-                }
-                else{
-                    imageButtons[i].setAlpha(0.3F);
-                    imageButtons[i].setOnClickListener(view ->{
-                        Toast.makeText(ResumoViagemActivity.this, R.string.tarifaAereaAviso, Toast.LENGTH_SHORT).show();
-                    });
-                }
-            }
+            defineFuncaoArrow(imageButtons[i], hiddenViews[i], cardViews[i], map.get(i));
 
-            else if(i == 2) {
-                if(refeicoesModel.getAdicionouViagem() != 0) {
-                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i]);
-                }
-                else{
-                    imageButtons[i].setAlpha(0.3F);
-                    imageButtons[i].setOnClickListener(view ->{
-                        Toast.makeText(ResumoViagemActivity.this, R.string.refeicoesAviso, Toast.LENGTH_SHORT).show();
-                    });
-                }
-            }
+//            else if(i == 1) {
+//                if(tarifaAereaModel.getAdicionouViagem() != 0) {
+//                    setDadosTarifasAereas();
+//                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i], map.get(i));
+//                }
+//                else{
+//                    imageButtons[i].setAlpha(0.3F);
+//                    imageButtons[i].setOnClickListener(view ->{
+//                        Toast.makeText(ResumoViagemActivity.this, R.string.tarifaAereaAviso, Toast.LENGTH_SHORT).show();
+//                    });
+//                }
+//            }
+//
+//            else if(i == 2) {
+//                if(refeicoesModel.getAdicionouViagem() != 0) {
+//                    setDadosRefeicoes();
+//                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i], map.get(i));
+//                }
+//                else{
+//                    imageButtons[i].setAlpha(0.3F);
+//                    imageButtons[i].setOnClickListener(view ->{
+//                        Toast.makeText(ResumoViagemActivity.this, R.string.refeicoesAviso, Toast.LENGTH_SHORT).show();
+//                    });
+//                }
+//            }
+//
+//            else if(i == 3) {
+//                if(hospedagemModel.getAdicionouViagem() != 0) {
+//                    setDadoshospedagem();
+//                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i], map.get(i));
+//                }
+//                else{
+//                    imageButtons[i].setAlpha(0.3F);
+//                    imageButtons[i].setOnClickListener(view ->{
+//                        Toast.makeText(ResumoViagemActivity.this, R.string.hospedagemAviso, Toast.LENGTH_SHORT).show();
+//                    });
+//                }
+//            }
+//
+//            else if(i == 4) {
+//                if(entretenimentoModel.getAdicionouViagem() != 0) {
+//                    setDadosEntreterimento();
+//                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i], map.get(i));
+//                }
+//                else{
+//                    imageButtons[i].setAlpha(0.3F);
+//                    imageButtons[i].setOnClickListener(view ->{
+//                        Toast.makeText(ResumoViagemActivity.this, R.string.entreterimentoAviso, Toast.LENGTH_SHORT).show();
+//                    });
+//                }
+//            }
+        }
+    }
 
-            else if(i == 3) {
-                if(hospedagemModel.getAdicionouViagem() != 0) {
-                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i]);
-                }
-                else{
-                    imageButtons[i].setAlpha(0.3F);
-                    imageButtons[i].setOnClickListener(view ->{
-                        Toast.makeText(ResumoViagemActivity.this, R.string.hospedagemAviso, Toast.LENGTH_SHORT).show();
-                    });
-                }
+    private void defineFuncaoArrow(ImageButton arrow, LinearLayout hiddenView, CardView cardView, Object model){
+        if(model instanceof GasolinaModel){
+            if(gasolinaModel.getAdicionouViagem() != 0) {
+                setDadosGasolina();
+                expandeCardView(arrow, hiddenView, cardView);
             }
-
-            else if(i == 4) {
-                if(entretenimentoModel.getAdicionouViagem() != 0) {
-                    expandeCardView(imageButtons[i], hiddenViews[i], cardViews[i]);
-                }
-                else{
-                    imageButtons[i].setAlpha(0.3F);
-                    imageButtons[i].setOnClickListener(view ->{
-                        Toast.makeText(ResumoViagemActivity.this, R.string.entreterimentoAviso, Toast.LENGTH_SHORT).show();
-                    });
-                }
+            else{
+                arrow.setAlpha(0.3F);
+                arrow.setOnClickListener(view ->{
+                    Toast.makeText(ResumoViagemActivity.this, R.string.gasolinaAviso, Toast.LENGTH_SHORT).show();
+                });
+            }
+        } else if(model instanceof TarifaAereaModel){
+            if(tarifaAereaModel.getAdicionouViagem() != 0) {
+                setDadosTarifasAereas();
+                expandeCardView(arrow, hiddenView, cardView);
+            }
+            else{
+                arrow.setAlpha(0.3F);
+                arrow.setOnClickListener(view ->{
+                    Toast.makeText(ResumoViagemActivity.this, R.string.tarifaAereaAviso, Toast.LENGTH_SHORT).show();
+                });
+            }
+        } else if(model instanceof RefeicoesModel){
+            if(refeicoesModel.getAdicionouViagem() != 0) {
+                setDadosRefeicoes();
+                expandeCardView(arrow, hiddenView, cardView);
+            }
+            else{
+                arrow.setAlpha(0.3F);
+                arrow.setOnClickListener(view ->{
+                    Toast.makeText(ResumoViagemActivity.this, R.string.refeicoesAviso, Toast.LENGTH_SHORT).show();
+                });
+            }
+        }else if(model instanceof HospedagemModel){
+            if(hospedagemModel.getAdicionouViagem() != 0) {
+                setDadoshospedagem();
+                expandeCardView(arrow, hiddenView, cardView);
+            }
+            else{
+                arrow.setAlpha(0.3F);
+                arrow.setOnClickListener(view ->{
+                    Toast.makeText(ResumoViagemActivity.this, R.string.hospedagemAviso, Toast.LENGTH_SHORT).show();
+                });
+            }
+        } else if(model instanceof EntretenimentoModel){
+            if(entretenimentoModel.getAdicionouViagem() != 0) {
+                setDadosEntreterimento();
+                expandeCardView(arrow, hiddenView, cardView);
+            }
+            else{
+                arrow.setAlpha(0.3F);
+                arrow.setOnClickListener(view ->{
+                    Toast.makeText(ResumoViagemActivity.this, R.string.entreterimentoAviso, Toast.LENGTH_SHORT).show();
+                });
             }
         }
 
-        setDadosGasolina();
     }
 
     private void expandeCardView(ImageButton arrow, LinearLayout hiddenView, CardView cardView){
@@ -176,6 +237,29 @@ public class ResumoViagemActivity extends AppCompatActivity {
         txtMediaKmsLitro = findViewById(R.id.resumo_mediaKmsLitro);
         txtCustoMedioLitro = findViewById(R.id.resumo_custoMedioLitro);
         txtTotalVeiculos = findViewById(R.id.resumo_totalVeiculos);
+        txtCustoPorPessoa = findViewById(R.id.resumo_custoEstimadoPessoa);
+        txtAluguelVeiculo = findViewById(R.id.resumo_aluguelVeiculos);
+        txtCustoRefeicao = findViewById(R.id.resumo_custoEstimadoRefeicao);
+        txtRefeicoesDia = findViewById(R.id.resumo_refeicoesPorDia);
+        txtCustoMedioNoite = findViewById(R.id.resumo_custoMedioNoite);
+        txtTotalNoites = findViewById(R.id.resumo_totalNoites);
+        txtTotalQuartos = findViewById(R.id.resumo_totalQuartos);
+        txtValorEntreterimento1 = findViewById(R.id.resumo_valorEntreterimento1);
+        txtValorEntreterimento2 = findViewById(R.id.resumo_valorEntreterimento2);
+        txtValorEntreterimento3 = findViewById(R.id.resumo_valorEntreterimento3);
+        txtEntreterimento1 = findViewById(R.id.resumo_txtEntreterimento1);
+        txtEntreterimento2 = findViewById(R.id.resumo_txtEntreterimento2);
+        txtEntreterimento3 = findViewById(R.id.resumo_txtEntreterimento3);
+        txtTotalFinalGasolina = findViewById(R.id.resumo_totalFinalGasolina);
+        txtTotalFinalTarifasAereas = findViewById(R.id.resumo_totalFinalTarifasAereas);
+        txtTotalFinalRefeicoes = findViewById(R.id.resumo_totalFinalRefeicoes);
+        txtTotalFinalHospedagem = findViewById(R.id.resumo_totalFinalHospedagem);
+        txtTotalFinalEntreterimento = findViewById(R.id.resumo_totalFinalEntreterimento);
+        resumoLinearEnt1 = findViewById(R.id.resumo_LinearEnt1);
+        resumoLinearEnt2 = findViewById(R.id.resumo_LinearEnt2);
+        resumoLinearEnt3 = findViewById(R.id.resumo_LinearEnt3);
+
+
     }
 
     private void setaVariaveisExtras() {
@@ -192,6 +276,13 @@ public class ResumoViagemActivity extends AppCompatActivity {
         refeicoesDAO = new RefeicoesDAO(context);
         hospedagemDAO = new HospedagemDAO(context);
         entretenimentoDAO = new EntretenimentoDAO(context);
+
+        map = new HashMap<>();
+        map.put(0, gasolinaModel);
+        map.put(1, tarifaAereaModel);
+        map.put(2, refeicoesModel);
+        map.put(3, hospedagemModel);
+        map.put(4, entretenimentoModel);
     }
 
     private void setModels(String idViagem){
@@ -205,10 +296,53 @@ public class ResumoViagemActivity extends AppCompatActivity {
 
     private void setDadosGasolina(){
         txtTotalKms.setText(String.valueOf(gasolinaModel.getTotalEstimadoKms()));
-        txtMediaKmsLitro.setText(String.valueOf(gasolinaModel.getCustoMedioLitro()));
+        txtMediaKmsLitro.setText(String.valueOf(gasolinaModel.getMediaKmsLitro()));
         txtCustoMedioLitro.setText(String.valueOf(gasolinaModel.getCustoMedioLitro()));
         txtTotalVeiculos.setText(String.valueOf(gasolinaModel.getTotalVeiculos()));
+        txtTotalFinalGasolina.setText(String.valueOf(gasolinaModel.getTotal()));
+    }
 
+    private void setDadosTarifasAereas(){
+        txtCustoPorPessoa.setText(String.valueOf(tarifaAereaModel.getCustoEstimadoPessoa()));
+        txtAluguelVeiculo.setText(String.valueOf(tarifaAereaModel.getAluguelVeiculo()));
+        txtTotalFinalTarifasAereas.setText(String.valueOf(tarifaAereaModel.getTotal()));
+    }
+
+    private void setDadoshospedagem(){
+        txtCustoMedioNoite.setText(String.valueOf(hospedagemModel.getCustoMedioNoite()));
+        txtTotalNoites.setText(String.valueOf(hospedagemModel.getTotalNoites()));
+        txtTotalQuartos.setText(String.valueOf(hospedagemModel.getTotalQuartos()));
+        txtTotalFinalHospedagem.setText(String.valueOf(hospedagemModel.getTotal()));
+    }
+
+    private void setDadosRefeicoes(){
+        txtCustoRefeicao.setText(String.valueOf(refeicoesModel.getCustoEstimadoRefeicao()));
+        txtRefeicoesDia.setText(String.valueOf(refeicoesModel.getRefeicoesDia()));
+        txtTotalFinalRefeicoes.setText(String.valueOf(refeicoesModel.getTotal()));
+    }
+
+    private void setDadosEntreterimento(){
+
+            txtEntreterimento1.setText(String.valueOf(entretenimentoModel.getEntretenimento1()));
+            txtEntreterimento2.setText(String.valueOf(entretenimentoModel.getEntretenimento2()));
+            txtEntreterimento3.setText(String.valueOf(entretenimentoModel.getEntretenimento3()));
+            txtValorEntreterimento1.setText((String.valueOf(entretenimentoModel.getValorEntretenimento1())));
+            txtValorEntreterimento2.setText((String.valueOf(entretenimentoModel.getValorEntretenimento2())));
+            txtValorEntreterimento3.setText((String.valueOf(entretenimentoModel.getValorEntretenimento3())));
+
+            if (entretenimentoModel.getEntretenimento1().equals("")) {
+                resumoLinearEnt1.setVisibility(View.GONE);
+            }
+
+            if (entretenimentoModel.getEntretenimento2().equals("")) {
+                resumoLinearEnt2.setVisibility(View.GONE);
+            }
+
+            if (entretenimentoModel.getEntretenimento3().equals("")) {
+                resumoLinearEnt3.setVisibility(View.GONE);
+            }
+
+            txtTotalFinalEntreterimento.setText(String.valueOf(entretenimentoModel.getTotal()));
     }
 
     @Override
